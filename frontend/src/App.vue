b9ea110a8800cad446f095df176b7befc0c127f6
Grant Gonzalez <grantgonzalez14@gmail.com>
2022-10-02 14:15:07 -0400

    Restructured project to include all vue3 functionality

---
+++ frontend/src/App.vue
@@ -0,0 +1,25 @@
+<template>
+  <login-form></login-form>
+</template>
+
+<script>
+import loginForm from './components/loginForm.vue'
+
+export default {
+  name: 'App',
+  components: {
+    'login-form': loginForm
+  }
+}
+</script>
+
+<style>
+html, body {
+  height: 100%;
+}
+
+body {
+  display: grid;
+  place-items: center;
+}
+</style>
---
+++ frontend/src/components/loginForm.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="login">
+    <h2>LOGIN</h2>
+    <form>
+      <label for="username">Username:&emsp;</label>
+      <input type="text" id="username" name="username" required><br><br>
+      <label for="password">Password: &emsp;</label>
+      <input type="text" id="password" name="password" required><br><br>
+      <input type="submit" id="login-button" value="Login">
+    </form>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'login-form'
+}
+</script>
+
+<style>
+.login {
+  display: grid;
+  place-items: center;
+  padding: 0 10px 10px;
+  border-style: solid;
+  border-color: black;
+}
+
+#login-button {
+  width: 100%;
+}
+</style>
