29aee8660a8493031c6f5f6e05bc21d84560370f
Grant David Gonzalez <gdgonza@clemson.edu>
2022-10-03 17:41:23 +0000

    Updated index.js

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -15,17 +15,17 @@ const routes = [
         redirect: 'https://spacebarcowboysbackend.herokuapp.com/login'
     },
     {
-        path: '/driver',
+        path: 'https://spacebarcowboysbackend.herokuapp.com/driver',
         name: 'driver-dashboard',
         component: DriverDashboard
     },
     {
-        path: '/admin',
+        path: 'https://spacebarcowboysbackend.herokuapp.com/admin',
         name: 'admin-dashboard',
         component: AdminDashboard
     },
     {
-        path: '/sponsor',
+        path: 'https://spacebarcowboysbackend.herokuapp.com/sponsor',
         name: 'sponsor-dashboard',
         component: SponsorDashboard
     }
