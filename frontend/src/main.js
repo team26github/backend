81e390af2a4ab35387399c8d833c524b8b739f13
kjpierc <kjpierc@clemson.edu>
2022-10-11 15:17:45 -0400

    backend setup

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,5 +1,13 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router/index.js';
+import { DataStore } from '@aws-amplify/datastore';
+import { Todo } from './models';
 
 createApp(App).use(router).mount('#app');
+
+await DataStore.save(
+    new Todo({
+		"Description": "Lorem ipsum dolor sit amet"
+	})
+);
