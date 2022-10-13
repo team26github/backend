6c658e9e03b1a842cfd41e968162723deefafb31
Grant Gonzalez <grantgonzalez14@gmail.com>
2022-10-13 17:21:22 -0400

    sync

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,13 +1,15 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router/index.js';
-import { DataStore } from '@aws-amplify/datastore';
-import { Todo } from './models';
+/*import { DataStore } from '@aws-amplify/datastore';*/
+/*import { Todo } from './models';*/
 
 createApp(App).use(router).mount('#app');
 
+/*
 await DataStore.save(
     new Todo({
 		"Description": "Lorem ipsum dolor sit amet"
 	})
 );
+*/
