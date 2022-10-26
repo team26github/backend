ddbb51017c5ea17eb2d7e477cefc133e45b87696
Grant Gonzalez <grantgonzalez14@gmail.com>
2022-10-26 08:47:33 -0400

    Full name now appears

--- frontend/src/components/AdminSettings.vue
+++ frontend/src/components/AdminSettings.vue
@@ -10,7 +10,7 @@
                 <label class="drivers">Choose a Driver:</label>
                 <select class="drivers">
                     <option value="All" selected>All</option>
-                    <option v-for="driver in driver_info" :key="driver">{{ driver[4] }}</option>
+                    <option v-for="driver in driver_info" :key="driver" :value="driver[9]" @change="get_driver($event)">{{ driver[9] }}</option>
                 </select>
                 <button @click="show_drivers = !show_drivers">{{ (show_drivers ? "Hide" : "View") }}</button>
             </div>
@@ -21,7 +21,7 @@
                 <label class="sponsors">Choose a Sponsor:</label>
                 <select class="sponsors">
                     <option value="All" selected>All</option>
-                    <option v-for="sponsor in sponsor_info" :key="sponsor">{{ sponsor[4] }}</option>
+                    <option v-for="sponsor in sponsor_info" :key="sponsor" :value="sponsor[9]" @change="get_sponsor($event)">{{ sponsor[9] }}</option>
                 </select>
                 <button @click="show_sponsors = !show_sponsors">{{ (show_sponsors ? "Hide" : "View") }}</button>
             </div>
@@ -32,43 +32,43 @@
                 <label class="admins">Choose an Admin:</label>
                 <select class="admins">
                     <option value="All" selected>All</option>
-                    <option v-for="admin in admin_info" :key="admin">{{ admin[4] }}</option>
+                    <option v-for="admin in admin_info" :key="admin" :value="admin[9]" @change="get_admin($event)">{{ admin[9] }}</option>
                 </select>
                 <button @click="show_admins = !show_admins;">{{ (show_admins ? "Hide" : "View") }}</button>
             </div>
         </div>
-        <div class="row">
+        <div class="info-row">
             <div class="driver-container" v-if="show_drivers">
                 <div class="driver-info" v-for="driver in driver_info" :key="driver">
-                    <p><strong>Full Name:</strong></p>
+                    <p><strong>Full Name:</strong> {{ driver[9] }}</p>
                     <p><strong>Username:</strong> {{ driver[4] }}</p>
                     <p><strong>Email:</strong> {{ driver[3] }}</p>
-                    <p><strong>Points Limit:</strong> {{ driver[5] }}</p>
-                    <p><strong>Expiration Period:</strong> {{ driver[6] }}</p>
-                    <p><strong>Sponsor(s):</strong> {{ driver[7] }}</p>
-                    <p><strong>Point Conversion:</strong> {{ driver[8] }}</p>
+                    <p><strong>Points Limit:</strong> {{ driver[5] }} points</p>
+                    <p><strong>Expiration Period:</strong> {{ driver[6] }} months</p>
+                    <p><strong>Sponsor ID(s):</strong> {{ driver[7] }}</p>
+                    <p><strong>Point Conversion:</strong> {{ driver[8] }} points = $1</p>
                 </div>
             </div>
             <div class="sponsor-container" v-if="show_sponsors">
                 <div class="sponsor-info" v-for="sponsor in sponsor_info" :key="sponsor">
-                    <p><strong>Full Name:</strong></p>
+                    <p><strong>Full Name:</strong> {{ sponsor[9] }}</p>
                     <p><strong>Username:</strong> {{ sponsor[4] }}</p>
                     <p><strong>Email:</strong> {{ sponsor[3] }}</p>
-                    <p><strong>Points Limit:</strong> {{ sponsor[5] }}</p>
-                    <p><strong>Expiration Period:</strong> {{ sponsor[6] }}</p>
-                    <p><strong>Sponsor(s):</strong> {{ sponsor[7] }}</p>
-                    <p><strong>Point Conversion:</strong> {{ sponsor[8] }}</p>
+                    <p><strong>Points Limit:</strong> {{ sponsor[5] }} points</p>
+                    <p><strong>Expiration Period:</strong> {{ sponsor[6] }} months</p>
+                    <p><strong>Sponsor ID(s):</strong> {{ sponsor[7] }}</p>
+                    <p><strong>Point Conversion:</strong> {{ sponsor[8] }} points = $1</p>
                 </div>
             </div>
             <div class="admin-container" v-if="show_admins">
                 <div class="admin-info" v-for="admin in admin_info" :key="admin">
-                    <p><strong>Full Name:</strong></p>
+                    <p><strong>Full Name:</strong> {{ admin[9] }}</p>
                     <p><strong>Username:</strong> {{ admin[4] }}</p>
                     <p><strong>Email:</strong> {{ admin[3] }}</p>
-                    <p><strong>Points Limit:</strong> {{ admin[5] }}</p>
-                    <p><strong>Expiration Period:</strong> {{ admin[6] }}</p>
-                    <p><strong>Sponsor(s):</strong> {{ admin[7] }}</p>
-                    <p><strong>Point Conversion:</strong> {{ admin[8] }}</p>
+                    <p><strong>Points Limit:</strong> {{ admin[5] }} points</p>
+                    <p><strong>Expiration Period:</strong> {{ admin[6] }} months</p>
+                    <p><strong>Sponsor ID(s):</strong> {{ admin[7] }}</p>
+                    <p><strong>Point Conversion:</strong> {{ admin[8] }} points = $1</p>
                 </div>
             </div>
         </div>
@@ -91,6 +91,9 @@
             show_drivers: false,
             show_sponsors: false,
             show_admins: false,
+            selected_driver: "All",
+            selected_sponsor: "All",
+            selected_admin: "All",
             production_path: "http://18.191.136.200",
             localhost_path: "http://localhost:5000",
             path: null
@@ -124,6 +127,20 @@
             })
     },
 
+    methods: {
+        get_driver(event) {
+            this.selected_driver = event.target.value;
+        },
+        
+        get_sponsor(event) {
+            this.selected_sponsor = event.target.value;
+        },
+
+        get_admin(event) {
+            this.selected_admin = event.target.value;
+        }
+    },
+
     components: { NavBar }
 }
 </script>
@@ -143,6 +160,15 @@
     .row {
         width: 100%;
         height: 45px;
+        display: flex;
+        flex-direction: row;
+        justify-content: center;
+        gap: 2rem;
+    }
+
+    .info-row {
+        width: 100%;
+        height: 50vh;
         display: flex;
         flex-direction: row;
         justify-content: center;
@@ -168,7 +194,8 @@
         display: flex;
         flex-direction: column;
         width: 30%;
-        height: 67vh;
+        height: 100%;
+        overflow-y: auto;
         border-style: solid;
         border-color: black;
         align-items: center;
