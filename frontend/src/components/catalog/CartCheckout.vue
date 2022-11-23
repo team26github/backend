332b441a9e6e499d047263ba1c72aadf55ee74cf
Grant Gonzalez <grantgonzalez14@gmail.com>
2022-11-23 13:56:11 -0500

    Added explanation comments to cart/cart checkout code

--- frontend/src/components/catalog/CartCheckout.vue
+++ frontend/src/components/catalog/CartCheckout.vue
@@ -4,49 +4,61 @@
         <form style="max-width:800px;margin:auto">
             <h1>Checkout</h1>
 
+            <!-- Section to display catalog items in the user's cart -->
             <div class="row">
                 <div class="catalog-items">
                     <ul><strong>Cart Items:</strong></ul>
+
+                    <!-- A list item renders for each item in the cart -->
                     <li v-for="item in items" :key="item">&nbsp;{{ item }}</li>
                 </div>
             </div>
 
             <p>Please enter the below information to complete your purchase.</p>
 
+            <!-- Input field for First Name -->
             <div class="input-container">
                 <input class="input-field" type="text" placeholder="First Name" name="first_name" v-model="first_name" required><br><br>
             </div>
 
+            <!-- Input field for Last Name -->
             <div class="input-container">
                 <input class="input-field" type="text" placeholder="Last Name" name="last_name" v-model="last_name" required><br><br>
             </div>
 
+            <!-- Input field for Address -->
             <div class="input-container">
                 <input class="input-field" type="text" placeholder="Address" name="address" v-model="address" required><br><br>
             </div>
 
+            <!-- Input field for City -->
             <div class="input-container">
                 <input class="input-field" type="text" placeholder="City" name="city" v-model="address_city" required><br><br>
             </div>
 
+            <!-- Input field for State Abbreviation -->
             <div class="input-container">
                 <input class="input-field" type="text" placeholder="State Abbreviation" name="state" v-model="address_state" required><br><br>
             </div>
 
+            <!-- Input field for Zip Code -->
             <div class="input-container">
                 <input class="input-field" type="text" placeholder="Zip Code" name="zip_code" v-model="address_zip_code" required><br><br>
             </div>
 
+            <!-- Input field for Email -->
             <div class="input-container">
                 <input class="input-field" type="text" placeholder="Email" name="email" v-model="email" required><br><br>
             </div>
 
+            <!-- Submit button -->
             <button id="myBtn" class="btn" :disabled="!sufficient_balance" @click="submit_purchase()">
                 Pay {{ this.points_total }} points
             </button>
 
             <br><br>
 
+            <!-- Button to send user back to their dashboard -->
             <button id="myBtn" @click="return_to_dashboard()">
                 Return to Driver Dashboard
             </button>
@@ -61,8 +73,11 @@
     import NavBar from '@/components/misc/NavBar.vue';
 
     export default {
+
+        // Component name
         name: "cart-checkout",
 
+        // Component specific variables and data
         data() {
             return {
                 status: null,
@@ -88,19 +103,30 @@
             };
         },
 
+        // Mounted function is used for doing operations right after the component
+        // Is mounted and right before the component is shown to the user
         mounted() {
+
+            // Getting username, cart items, and total cost from url
             this.username = this.$route.params.username;
             this.items = JSON.parse(this.$route.params.cart);
             this.points_total = this.$route.params.cost;
+
+            // Getting the number of items in the cart
             this.items_total = this.items.length;
+
+            // Setting Axios API path to either local host or production
             this.path = this.localhost_path;
 
+            // Axios API call to python backend to get user information from the database
             axios.get(this.path + '/userinfo', {params: {username: this.username}})
                 .then((res) => {
                     if (res.data.status === 'success') {
                         this.user_id = res.data.results[0][0];
                         this.user_type = res.data.results[0][2];
                         this.points_balance = res.data.results[0][11];
+
+                        // Determining if the user has enough points to purchase their items
                         if ( (this.points_balance < this.points_total) || (0 == this.points_total) ){
                             this.sufficient_balance = false;
                         }
@@ -117,6 +143,7 @@
                 });
         },
 
+        // Component specific methods
         methods: {
 
             return_to_dashboard() {
@@ -124,6 +151,8 @@
             },
 
             submit_purchase() {
+
+                // Axios API call to python backend to add purchase information to the database
                 axios.post(this.path + '/submit-purchase', null, {params: {first_name: this.first_name, last_name: this.last_name, address: this.address, address_city: this.address_city, address_state: this.address_state, address_zip_code: this.address_zip_code, email: this.email, items: JSON.stringify(this.items), items_total: this.items_total, points_total: this.points_total, user_id: this.user_id }}) 
                     .then((res) => {
                         if (res.data.status === "success") {
@@ -139,7 +168,10 @@
                     });
             },
 
+            // Method to remove points from the user after a purchase has been completed
             remove_points_purchase() {
+
+                // Axios API call to python backend to update user point information in the database
                 axios.post(this.path + 'remove_points_purchase', null, {params: {points_total: this.points_total, reason: this.reason, email: this.email}}) 
                     .then((res) => {
                         if (res.data.status === "success") {
@@ -155,13 +187,18 @@
                     })
             },
 
-            get_points_balance(){ //show records
+            // Method to get a user's point balance
+            get_points_balance(){
+
+                // Axios API call to python backend to get the user's point balance
                 axios.get(this.path + '/userinfo', {params: {request: 'username'}})
                 .then(function(response){
                         this.points_balance = response.data.results[0][11];
                 });
             },
         },
+
+        // Components used from external files
         components: { NavBar }
     }
 
--- frontend/src/components/catalog/Catalog.vue
+++ frontend/src/components/catalog/Catalog.vue
@@ -1,17 +1,29 @@
 <template>
     <NavBar :usertype="user_type" :username="username"></NavBar>
+    <!-- Row to hold options for the user such as filters and item searching -->
     <div class="options">
+
+        <!-- Filters dropdown menu based on what the sponsor specifications are -->
         <label>Filters:</label>
         <select class="filters" @change="get_filter($event)"> 
             <option value="All" selected>All</option>
             <option v-for="filter in catalog_filters" :key="filter">{{ filter }}</option>
         </select>
+
+        <!-- Search box and button for the user to search for specific items -->
         <input class="filters" type="text" placeholder="Search" v-model="search_text"/>
         <button type="submit" @click="search()">Search</button>
+
+        <!-- Button to send user to checkout screen and number showing how many items are in their cart -->
         <button class="cart" @click="checkout()">Checkout</button>
         <p class="cart">Items in Cart: {{ num_in_cart }}</p>
     </div>
+
+    <!-- Container to hold items in the catalog -->
     <div class="catalog-container" :key="update">
+
+        <!-- Item(s) in the catalog available for user purchase. If there are more than one catalog item available
+            for purchase, then multiple containers will be rendered -->
         <div v-for="item in catalog_items" :key="item" class="catalog-item-container">
             <img class="item-img" v-if="'image' in item" :src="item.image.imageUrl"/>
             <p><strong>Item:</strong> {{item.title}}</p>
@@ -27,111 +39,73 @@
     import axios from 'axios';
 
     export default {
-    name: "user-catalog",
-    data() {
-        return {
-            user_type: null,
-            user_id: null,
-            username: null,
-            catalog_filters: null,
-            selected_filter: null,
-            search_text: null,
-            point_conversion: null,
-            update: 0,
-            cart: [],
-            cost: 0.0,
-            num_in_cart: 0,
-            catalog_items: null,
-            production_path: "http://18.191.136.200",
-            localhost_path: "http://localhost:5000",
-            path: null
-        };
-    },
 
-    mounted() {
-        this.username = this.$route.params.username;
-        this.path = this.localhost_path;
+        // Component name
+        name: "user-catalog",
 
-        axios.get(this.path + '/userinfo', {params: {username: this.username}})
-                .then((res) => {
-                    if (res.data.status === 'success') {
-                        this.user_id = res.data.results[0][0];
-                        this.user_type = res.data.results[0][2];
-                        this.point_conversion = res.data.results[0][8];
-                        this.catalog_filters = res.data.results[0][10].split(',');
-                        this.get_catalog_items();
-                    }
-                    else {
-                        console.log('Unsuccessful');
-                    }
-                })
-                .catch((error) => {
-                    console.log(error);
-                })
-    },
-
-    methods: {
-        get_filter(event) {
-            this.selected_filter = event.target.value;
-            this.get_catalog_items();
-            this.update += 1;
+        // Component specific variables and data
+        data() {
+            return {
+                user_type: null,
+                user_id: null,
+                username: null,
+                catalog_filters: null,
+                selected_filter: null,
+                search_text: null,
+                point_conversion: null,
+                update: 0,
+                cart: [],
+                cost: 0.0,
+                num_in_cart: 0,
+                catalog_items: null,
+                production_path: "http://18.191.136.200",
+                localhost_path: "http://localhost:5000",
+                path: null
+            };
         },
 
-        get_catalog_items() {
-            let keywords = '';
-            this.catalog_items = [];
+        // Mounted function is used for doing operations right after the component
+        // Is mounted and right before the component is shown to the user
+        mounted() {
+            this.username = this.$route.params.username;
+            this.path = this.localhost_path;
 
-            if (this.selected_filter === null || this.selected_filter === 'All') {
-                for (let i = 0; i < this.catalog_filters.length - 1; i++) {
-                    keywords += this.catalog_filters[i] + ',';
-                }
-                
-                keywords += this.catalog_filters[this.catalog_filters.length - 1];
-            }
-            else keywords += this.selected_filter;
-
-            axios.get(this.path + '/get-catalog-items', {params: {keywords: keywords}})
-                .then((res) => {
-                    let results = res.data.results;
-                    let temp = res.data.purchased;
-                    let purchased = [];                    
-                    
-                    for (let i = 0; i < temp.length; i++) {
-                        temp[i][0] = temp[i][0].split(/{\d: |'|"|}|, \d: /g);
-                    }
-
-                    for (let i = 0; i < temp.length; i++) {
-                        for (let j = 0; j < temp[0].length; j++) {
-                            for (let k = 0; k < temp[i][j].length; k++) {
-                                if (temp[i][j][k] !== "" && temp[i][j][k] !== " ") {
-                                    purchased.push(temp[i][j][k]);
-                                }
-                            }
+            // Axios API call to python backend to get user information from the database
+            axios.get(this.path + '/userinfo', {params: {username: this.username}})
+                    .then((res) => {
+                        if (res.data.status === 'success') {
+                            this.user_id = res.data.results[0][0];
+                            this.user_type = res.data.results[0][2];
+                            this.point_conversion = res.data.results[0][8];
+                            this.catalog_filters = res.data.results[0][10].split(',');
+                            this.get_catalog_items();
                         }
-                    }
-                    console.log(purchased);
-
-                    for (let item of results) {
-                        let add = true;
-                        for (let i = 0; i < purchased.length; i++) {
-                            if (item.title === purchased[i]) add = false;
+                        else {
+                            console.log('Unsuccessful');
                         }
-                        
-                        if (add) this.catalog_items.push(item);
-                        else add = true;
-                    }
-                })
-                .catch((err) => {
-                    console.log(err);
-                })            
+                    })
+                    .catch((error) => {
+                        console.log(error);
+                    })
         },
 
-        search() {
-            if (this.search_text !== null && this.search_text !== "") {
+        // Component specific methods
+        methods: {
 
+            // Getting user selected filters
+            get_filter(event) {
+                this.selected_filter = event.target.value;
+                this.get_catalog_items();
+                this.update += 1;
+            },
+
+            // Getting items for the catalog
+            get_catalog_items() {
                 let keywords = '';
                 this.catalog_items = [];
 
+                // If there is a filter applied, then the catalog will display only those
+                // items that meet the filter requirements
                 if (this.selected_filter === null || this.selected_filter === 'All') {
                     for (let i = 0; i < this.catalog_filters.length - 1; i++) {
                         keywords += this.catalog_filters[i] + ',';
@@ -141,44 +115,110 @@
                 }
                 else keywords += this.selected_filter;
 
+                // Axios API call to python backend to get catalog items from eBay
                 axios.get(this.path + '/get-catalog-items', {params: {keywords: keywords}})
                     .then((res) => {
                         let results = res.data.results;
+                        let temp = res.data.purchased;
+                        let purchased = [];                    
+                        
+                        // Getting previously purchased items so they do not show up
+                        // in the catalog display
+                        for (let i = 0; i < temp.length; i++) {
+                            temp[i][0] = temp[i][0].split(/{\d: |'|"|}|, \d: /g);
+                        }
 
-                        for (let item of results) {
-                            if (item.title.toLowerCase().includes(this.search_text.toLowerCase())) {
-                                this.catalog_items.push(item);
+                        // Adding purchases to purchased array
+                        for (let i = 0; i < temp.length; i++) {
+                            for (let j = 0; j < temp[0].length; j++) {
+                                for (let k = 0; k < temp[i][j].length; k++) {
+                                    if (temp[i][j][k] !== "" && temp[i][j][k] !== " ") {
+                                        purchased.push(temp[i][j][k]);
+                                    }
+                                }
                             }
                         }
-                        
-                        this.update += 1;
+
+                        // Adding catalog items to catalog_items array if they have not
+                        // already been purchased
+                        for (let item of results) {
+                            let add = true;
+                            for (let i = 0; i < purchased.length; i++) {
+                                if (item.title === purchased[i]) add = false;
+                            }
+                            
+                            if (add) this.catalog_items.push(item);
+                            else add = true;
+                        }
                     })
                     .catch((err) => {
                         console.log(err);
-                    })
+                    })            
+            },
+
+            // Method used for user search functionality
+            search() {
+
+                // If the search box is not empty
+                if (this.search_text !== null && this.search_text !== "") {
+
+                    let keywords = '';
+                    this.catalog_items = [];
+
+                    // If there is a filter as well as a search
+                    if (this.selected_filter === null || this.selected_filter === 'All') {
+                        for (let i = 0; i < this.catalog_filters.length - 1; i++) {
+                            keywords += this.catalog_filters[i] + ',';
+                        }
+                        
+                        keywords += this.catalog_filters[this.catalog_filters.length - 1];
+                    }
+                    else keywords += this.selected_filter;
+
+                    // Axios API call to python backend to get catalog items based on search and filter parameters
+                    axios.get(this.path + '/get-catalog-items', {params: {keywords: keywords}})
+                        .then((res) => {
+                            let results = res.data.results;
+
+                            // If the items from the catalog has the text from the search box included in
+                            // their title, then they are added to the catalog
+                            for (let item of results) {
+                                if (item.title.toLowerCase().includes(this.search_text.toLowerCase())) {
+                                    this.catalog_items.push(item);
+                                }
+                            }
+                            
+                            this.update += 1;
+                        })
+                        .catch((err) => {
+                            console.log(err);
+                        })
+                }
+            },
+
+            // Method to add items to the cart
+            add_to_cart(item) {
+
+                // If the item is not already in the cart, then it is added to the cart
+                if (this.cart.indexOf(item) === -1) {
+                    this.cart.push(item.title);
+                    this.num_in_cart += 1;
+                    this.cost += parseFloat((item.price.value * this.point_conversion).toFixed(2));
+                }
+            },
+
+            // Method to route user to checkout page with cart information
+            checkout() {
+                this.$router.push({
+                    name: 'cart-checkout',
+                    params: { username: this.username, cart: JSON.stringify(this.cart), cost: this.cost }
+                });
             }
         },
 
-        add_to_cart(item) {
-            if (this.cart.indexOf(item) === -1) {
-                this.cart.push(item.title);
-                this.num_in_cart += 1;
-                this.cost += parseFloat((item.price.value * this.point_conversion).toFixed(2));
-            }
-        },
-
-        checkout() {
-            //let content = { username: this.username, cart: this.cart, cost: this.cost };
-
-            this.$router.push({
-                name: 'cart-checkout',
-                params: { username: this.username, cart: JSON.stringify(this.cart), cost: this.cost }
-            });
-        }
-    },
-
-    components: { NavBar }
-}
+        // Components used from external files
+        components: { NavBar }
+    }
 </script>
 
 <style scoped>
